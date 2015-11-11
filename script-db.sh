e052704d07e34079f7bcf63f59effb88f8b5eaea
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-11 10:45:53 -0200

    #0 <@lucassveloso, @lucasviola> Adicionando script de instalação do postgres

---
+++ script-db.sh
@@ -0,0 +1,9 @@
+echo "Instalando Postgresql 9.3"
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-common
+sudo apt-get install postgresql-9.3 libpq-dev
+
+sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
+
