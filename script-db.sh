f6fd4ce09cda9e7fa3b863cb68b4e3feed653cdc
pedrofontoura <pedrofontoura93@gmail.com>
2015-11-17 10:45:30 -0200

    #0 <@lucassveloso, @lucasviola> - Alterando Script para verificar se o psql está instalado

--- script-db.sh
+++ script-db.sh
@@ -1,11 +1,21 @@
-echo "Instalando Postgresql 9.3"
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install postgresql-common
-sudo apt-get install postgresql-9.3 libpq-dev
 
+# 
+if [ -z "which psql" ]; then
+	
+	# Installing postgreSQL
+	echo "Installing Postgresql 9.3"
+	sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+	wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install postgresql-common
+	sudo apt-get install postgresql-9.3 libpq-dev
+
+fi
+
+# Creating User on PostgreSQL
 sudo -u postgres psql -c "CREATE USER $USER;"	
+
+# Granting privileges to $USER on CREATEDB
 sudo -u postgres psql -c "ALTER ROLE $USER WITH CREATEDB;"
 
 
