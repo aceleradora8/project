68f78f879880ff642c3ef9f9e1dd23e18384cff4
amandasv <amandasv.pm@gmail.com>
2015-11-18 15:55:44 -0200

    #T1 <@amandasv, @mibaratto, @ruas46> - Adicionando dependent: :detroy, para garantir que quando, por exemplo 1 ong for excluida, seu endereço, telefone e oportunidade, tambem serao excluidas

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -1,8 +1,8 @@
 class Ngo < ActiveRecord::Base
-	belongs_to :address
-	has_many :phones
+	belongs_to :address, dependent: :destroy
+	has_many :phones, dependent: :destroy
 	has_and_belongs_to_many :causes  	
-	has_one :opportunity
+	has_one :opportunity, dependent: :destroy
 	validates :address, :presence => true
 
 
