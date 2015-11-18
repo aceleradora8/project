d1b15f8fca220a34e05614816235e9c93a3006d4
amandasv <amandasv.pm@gmail.com>
2015-11-18 15:56:15 -0200

    #T1 <@amandasv, @mibaratto, @ruas46> - Descomentando o comentario de validate

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,7 +1,7 @@
 class Opportunity < ActiveRecord::Base	
 	belongs_to :address
 	belongs_to :ngo
-	belongs_to :cause	
+	belongs_to :cause
 	has_and_belongs_to_many :skills
 	validates :address, :presence => true
 	validates :ngo, :presence => true
