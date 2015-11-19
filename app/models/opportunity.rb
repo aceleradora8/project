c43a4fb7a38db67dae289d4e70b89d3c3926e70e
ruas46 <guilhermeruas@hotmail.com>
2015-11-19 11:18:03 -0200

    #T1<@ruas46,@mibaratto,@gabrielpozza> correção do script limpa e popula banco e da relação de endereço da tabela Opportunity

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,5 +1,5 @@
 class Opportunity < ActiveRecord::Base	
-	belongs_to :address
+	belongs_to :address, dependent: :destroy
 	belongs_to :ngo
 	belongs_to :cause
 	has_and_belongs_to_many :skills
--- db/scripts_teste/limpa_e_popula_banco.rb
+++ db/scripts_teste/limpa_e_popula_banco.rb
@@ -10,27 +10,45 @@ address_size = 5
 cause_size = 5
 ngo_size = 5
 opportunity_size = 5
+skill_size = 5
 
-#populando enderecos
+#populando enderecos para ongs
 (1..address_size).each do |index|
-	address = Address.new(id:index,address:"Rua Abcde",cep:"12123-123",city:"Porto Alegre", state:"Rio Grande do Sul",country:"Brasil")
+	address = Address.new(address:"Rua Abcde",cep:"12123-123",city:"Porto Alegre", state:"Rio Grande do Sul",country:"Brasil")
+	address.save
+end
+
+#populando enderecos para oportunidades
+(1..address_size).each do |index|
+	address = Address.new(address:"Rua oportunidades",cep:"12323-333",city:"Porto Alegre", state:"Rio Grande do Sul",country:"Brasil")
 	address.save
 end
 
 #populando causas
 (1..cause_size).each do |index|
-	cause = Cause.new(id:index,name:"causa #{index}")
+	cause = Cause.new(name:"causa #{index}")
 	cause.save
 end
 
 #populando ongs
 (1..ngo_size).each do |index|
-	ngo = Ngo.new(id:index,name:"Ong #{index}",description:"descr",address_id:index)
+	ngo = Ngo.new(name:"Ong #{index}",description:"descr",address_id:index)
 	ngo.save
 end
 
 #populando oportunidades
 (1..opportunity_size).each do |index|
-	opportunity = Opportunity.new(id:index,title:"oportunidade #{index}",description:"descr",address_id:index,ngo_id:index,cause_id:index,vacancies:666)
+	opportunity = Opportunity.new(title:"oportunidade #{index}",description:"descr",address_id:index+5,ngo_id:index,cause_id:index,vacancies:666)
 	opportunity.save
 end
+
+#populando skills
+(1..skill_size).each do |index|
+	skill = Skill.new(name:"nome skill #{index}")
+	skill.save
+end
+
+#criando relacao  oportunidade e skill
+op1 = Opportunity.first
+sk1 = Skill.first
+op1.skills.push(sk1) 
