a97547c73d6bd188ff0c22c5a8ca3b4fe3a7fed2
feliperdius <feliperdius@gmail.com>
2015-11-19 13:32:21 -0200

    #2 <@feliperdius, @raphaelbraun> Merge

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,14 +1,25 @@
 class OpportunitiesController < ApplicationController
+	before_action :set_opportunity, only: [:show]
 
 	def index
 		if params[:text_search] == nil || params[:text_search] == ""
-			@OpportunitySearch = Opportunity.all
+			@opportunity_search = Opportunity.all
 		else
-			@OpportunitySearch = Opportunity.search("#{params[:text_search] }").to_a
-			if @OpportunitySearch.empty? 
+			@opportunity_search = Opportunity.search("#{params[:text_search] }").to_a
+			if @opportunity_search.empty? 
 				@not_found = "Sua pesquisa não encontrou resultados"
 			end
 		end
 	end
+
+	def show
+	end
+
+
+	private 
+
+		def set_opportunity
+			@opportunity = Opportunity.find(params[:id])
+		end
 
 end
