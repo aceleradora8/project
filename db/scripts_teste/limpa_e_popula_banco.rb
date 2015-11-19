6140479448ea244cdd98d4da2859595ab541f7de
pedrocasarao <pedrohmprado@gmail.com>
2015-11-19 10:48:41 -0200

    #1 <@pedrocasarao, @amandasv> Testando a criação do banco no heroku

--- db/scripts_teste/limpa_e_popula_banco.rb
+++ db/scripts_teste/limpa_e_popula_banco.rb
@@ -13,24 +13,24 @@ opportunity_size = 5
 
 #populando enderecos
 (1..address_size).each do |index|
-	address = Address.new(id:index,address:"Rua Abcde",cep:"12123-123",city:"Porto Alegre", state:"Rio Grande do Sul",country:"Brasil")
+	address = Address.new(address:"Rua Abcde",cep:"12123-123",city:"Porto Alegre", state:"Rio Grande do Sul",country:"Brasil")
 	address.save
 end
 
 #populando causas
 (1..cause_size).each do |index|
-	cause = Cause.new(id:index,name:"causa #{index}")
+	cause = Cause.new(name:"causa #{index}")
 	cause.save
 end
 
 #populando ongs
 (1..ngo_size).each do |index|
-	ngo = Ngo.new(id:index,name:"Ong #{index}",description:"descr",address_id:index)
+	ngo = Ngo.new(name:"Ong #{index}",description:"descr",address_id:index)
 	ngo.save
 end
 
 #populando oportunidades
 (1..opportunity_size).each do |index|
-	opportunity = Opportunity.new(id:index,title:"oportunidade #{index}",description:"descr",address_id:index,ngo_id:index,cause_id:index,vacancies:666)
+	opportunity = Opportunity.new(title:"oportunidade #{index}",description:"descr",address_id:index,ngo_id:index,cause_id:index,vacancies:666)
 	opportunity.save
 end
