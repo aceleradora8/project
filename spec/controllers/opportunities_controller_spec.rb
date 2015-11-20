3f21cbac660d641a1bba6daf026913899bf25721
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-20 13:54:17 -0200

    #1 <@lucassveloso, @raphaelbraun, @pedrocasarao> - Implementando testes unitários da história Pesquisar Oportunidades

--- spec/controllers/opportunities_controller_spec.rb
+++ spec/controllers/opportunities_controller_spec.rb
@@ -1,17 +1,38 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe OpportunitiesController, type: :controller do
-#     describe "#index" do
-#         before :each do
-#             @opportunitiesController = OpportunitiesController.new
-#         end
+describe OpportunitiesController, type: :controller do
+    describe "#index" do
+        before :each do
+            address = Address.create!()
+        	ngo = Ngo.create!(address_id:address.id, email:"email")
+        	cause = Cause.create!()
+        	@opportunity1 = Opportunity.create!(title: "Opportunity1", address_id:address.id, ngo_id:ngo.id,cause_id:cause.id)
+        	@opportunity2 = Opportunity.create!(title: "Opportunity2", address_id:address.id, ngo_id:ngo.id,cause_id:cause.id)
+        end
 
-#         it "returns all opportunities when text_search is nil" do
-#             oppotunity = double(Opportunity)
-#             allow(oppotunity).to receive(:all) { "all opportunities" }
-#               get :index, :text_search => "teste"
-#             expect(oppotunity.all).to be == "all opportunities"
-#         end
-#     end
+        it "returns all opportunities when text_search is nil" do
+            get :index, text_search: nil
 
-# end
+            expect(assigns(:opportunity_search)).to match_array([@opportunity1,@opportunity2])
+        end
+
+        it "returns all opportunities when text_search is empty" do
+            get :index, text_search: ""
+
+            expect(assigns(:opportunity_search)).to match_array([@opportunity1,@opportunity2])
+        end
+
+        it "returns the opportunity when text_search matches the title" do
+            get :index, text_search: "Opportunity1"
+
+            expect(assigns(:opportunity_search)).to match_array([@opportunity1])
+        end
+
+        it "returns the opportunity when text_search matches the title" do
+            get :index, text_search: "Opportunity3"
+
+            expect(assigns(:not_found)).to be == "Sua pesquisa não encontrou resultados"
+        end
+    end
+
+end
