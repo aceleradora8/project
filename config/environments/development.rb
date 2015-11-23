2c9e1ae22b311ceae2d4369c7cbf8b043a9b4b96
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2015-11-23 13:24:04 -0200

    #3 <@amandasv, @ghig> - Deletando antigo mailer, criando um novo e modificando configs pro email teste

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'centrodeinovacaodememes',
+  password:             'lucasveloso',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
 end
