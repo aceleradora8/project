f2c3076d27e0dce0f55e5ed5961cd878c1b1658f
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-23 14:48:26 -0200

    #0 <@lucassveloso, @pedrocasarao, @ghig> - Resolvendo merge

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -6,23 +6,24 @@ class InterestsController < ApplicationController  # Prevent CSRF attacks by rai
 		@interest.build_volunteer
 	end
 
-	def create
-		@interest = Interest.new(interest_params)
+def create
+	@interest = Interest.new(interest_params)
 
-		respond_to do |format|
-			if @interest.save
-				InterestMailer.interest_email(@volunteer, @opportunity).deliver_later
-				NgoMailer.ngo_email(@volunteer, @opportunity).deliver_later
-				format.html { redirect_to @interest.opportunity, notice: 'Interesse registrado com sucesso' }
-			else
-				format.html { render :new }
-			end
+	respond_to do |format|
+		if @interest.save
+			InterestMailer.interest_email(@interest.volunteer, @interest.opportunity).deliver_later
+			NgoMailer.ngo_email(@interest.volunteer, @interest.opportunity).deliver_later
+			format.html { redirect_to @interest.opportunity, notice: 'Interesse registrado com sucesso' }
+		else
+			format.html { render :new }
 		end
-		end
+	end
+	end
 
 private
-	def interest_params
-		params.require(:interest).permit(:volunteer_attributes => [:name, :email, :phone, :observations], :opportunity_id, :confirmed, :presence)
-	end
+
+    def interest_params
+      params.require(:interest).permit(:opportunity_id, :volunteer_attributes => [:name, :email, :phone, :observations])
+    end
 
 end
