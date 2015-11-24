f5a1cd35a6dd1bf7962c2ee3ebe609dd7ce8e945
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-24 16:34:34 -0200

    #0 <@lucassveloso, @amandasv> - Renomeando teste de volunteer mailer

--- spec/mailers/volunteer_mailer_spec.rb
+++ spec/mailers/volunteer_mailer_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe InterestMailer, type: :mailer do
+RSpec.describe VolunteerMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
