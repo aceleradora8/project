c35188b6731edde27ed1193af991329fcbf9177b
pedrofontoura <pedrofontoura93@gmail.com>
2015-11-25 15:54:52 -0200

    #3 <@andrimenegotto, @pedrofontoura> - Implementação da validação do formulário de voluntário interessado em vaga

---
+++ app/helpers/error_message_for.rb
@@ -0,0 +1,7 @@
+
+error_messages_for(
+  :user, 
+  :header_message => "Oops - We couldn't save your user!", 
+  :message => "The following fields were a bit of a problem:", 
+  :header_tag => :h1
+)
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,3 +1,7 @@
 class Volunteer < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :phone, presence: true
+	
 	has_many :interests
 end
