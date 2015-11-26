f357d23ebba9457fe6f01e3909865c2be04d3ea8
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-26 09:24:58 -0200

    #0 <@lucassveloso, @raphaelbraun> - Removendo controller de volunteers e alguns helpers

--- app/helpers/error_message_for.rb
+++
@@ -1,7 +0,0 @@
-
-error_messages_for(
-  :user, 
-  :header_message => "Oops - We couldn't save your user!", 
-  :message => "The following fields were a bit of a problem:", 
-  :header_tag => :h1
-)
