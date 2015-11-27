f6fc5bd39b9c5b53adc5ef0e08a4373338a70aa7
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-27 15:35:00 -0200

    #0 <@lucassveloso, @pedrofontoura> - Refatorando método index de oportunidade

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,17 +1,16 @@
 class OpportunitiesController < ApplicationController
-	
-	before_action :set_opportunity, only: [:show,:interest]
+  before_action :set_opportunity, only: [:show,:interest]
   before_action :set_causes,:set_cities, only: [:index]
 
-	def index
-		if params[:text_search] == nil || params[:text_search] == ""
-			@opportunity_search = Opportunity.all.includes(:address,:ngo,:cause)
-		else
-			@opportunity_search = Opportunity.search("#{params[:text_search] }").includes(:address,:ngo,:cause)
-		end
-		respond_to do |format|
-	    if request.xhr? 
-  			@opportunities_result = [];
+  def index
+    if params[:text_search] == nil || params[:text_search] == ""
+      @opportunity_search = Opportunity.all.includes(:address,:ngo,:cause)
+    else
+      @opportunity_search = Opportunity.search("#{params[:text_search] }").includes(:address,:ngo,:cause)
+    end
+    respond_to do |format|
+      if request.xhr? 
+        @opportunities_result = [];
         if params[:causes] && params[:cities] 
           filter_with_causes_and_cities(params[:causes], params[:cities]);
         elsif params[:causes]
@@ -23,41 +22,41 @@ class OpportunitiesController < ApplicationController
         end
         @opportunities_result = Kaminari.paginate_array(@opportunities_result.flatten).page(params[:page])
         format.js
-    	else 
-    		@opportunities_result = Kaminari.paginate_array(@opportunity_search).page(params[:page])
-      	format.html
-	   	end
+      else 
+        @opportunities_result = Kaminari.paginate_array(@opportunity_search).page(params[:page])
+        format.html
+      end
     end
-	end
+  end
 
-	def new
-		@opportunity = Opportunity.new
-		@opportunity.build_address
-	end
+  def new
+    @opportunity = Opportunity.new
+    @opportunity.build_address
+  end
 
-	def create
-		@opportunity = Opportunity.new(opportunity_params)
-		@skill = Skill.all
-		respond_to do |format|
-			if @opportunity.save
-				format.html { redirect_to "/opportunities/#{@opportunity.id}" , notice: 'Oportunidade cadastrada com sucesso' }
-			else
-				format.html { render :new }
-			end
-		end
-	end
+  def create
+    @opportunity = Opportunity.new(opportunity_params)
+    @skill = Skill.all
+    respond_to do |format|
+      if @opportunity.save
+        format.html { redirect_to "/opportunities/#{@opportunity.id}" , notice: 'Oportunidade cadastrada com sucesso' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	private 
-		def set_opportunity
-			if Opportunity.where(:id => params[:id]).present?
-				@opportunity = Opportunity.find(params[:id])
-			else
-				redirect_to "/404"
-			end
-		end
+  private 
+    def set_opportunity
+      if Opportunity.where(:id => params[:id]).present?
+        @opportunity = Opportunity.find(params[:id])
+      else
+        redirect_to "/404"
+      end
+    end
 
     def set_causes
       @causes = Cause.all
@@ -67,9 +66,9 @@ class OpportunitiesController < ApplicationController
       @cities = Address.uniq.pluck(:city)
     end
 
-  	def opportunity_params
-  		params.require(:opportunity).permit(:id, :title, :description, :start_date, :finish_date, :ngo_id, :cause_id, :vacancies, :address_attributes => [:address, :cep, :complement, :state, :city, :country, :neighbourhood], :skill_ids => [])
-  	end
+    def opportunity_params
+      params.require(:opportunity).permit(:id, :title, :description, :start_date, :finish_date, :ngo_id, :cause_id, :vacancies, :address_attributes => [:address, :cep, :complement, :state, :city, :country, :neighbourhood], :skill_ids => [])
+    end
 
     def filter_with_causes_and_cities(causes, cities)
       causes.each do |cause|
