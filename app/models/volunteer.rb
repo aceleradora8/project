8a20892ad84f913099a342994af02466ed8c36f5
pedrofontoura <pedrofontoura93@gmail.com>
2015-11-27 11:18:10 -0200

    #0 <@lucassveloso, @pedrofontoura> - Corrigindo merge

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,7 +1,11 @@
 class Volunteer < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true
+	validates :email, format: { with: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    message: "Email inválido",:multiline => true }
 	validates :phone, presence: true
+	validates :phone, format: { with: /^([0-9]{2})?(\([0-9]{2})\)([0-9]{3}|[0-9]{4})-[0-9]{4}$/,
+    message: "Telefone inválido",:multiline => true}
 	
 	has_many :interests
 end
