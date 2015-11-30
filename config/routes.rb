9682beda5f0060614f64425f4f381d9f88b2f9e7
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2015-11-30 14:42:01 -0200

    #8 <@ghig, @amandasv, @lucassveloso> - Criando lógica de Autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   resources :ngos 
 
   get 'signup' => 'users#new'
-
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
