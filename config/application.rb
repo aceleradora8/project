031fb0ccb3fd55074aa7ca600f7a0439c9c91d67
andrimenegotto <andriele.menegotto@gmail.com>
2015-12-04 14:23:06 -0200

    #0 <@andrimenegotto, @amandasv> - Arrumando horário para testar recuperação de senha

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module OngariumRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pt_br
 
+    config.time_zone = "Brasilia"
+    config.active_record.default_timezone = :local
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
