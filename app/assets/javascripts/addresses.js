7b80d3a09a1f703a9ab1e2c682c4d22cc1c38c71
gabrielpozza <gabriel.pozza@acad.pucrs.br>
2015-12-07 16:00:33 -0200

    #6 <@pauladbol, @gabrielpozza, @andrimenegotto> correção da validação da addresses

---
+++ app/assets/javascripts/addresses.js
@@ -0,0 +1,47 @@
+var ADDRESSES = ADDRESSES || {}
+
+ADDRESSES.validateForm = function validateForm()
+{
+  if (VALIDATION.validateZipCode()) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+ADDRESSES.cleanFields = function cleanFields() {
+  $("#inputState").attr("text","");
+  $("#inputCity").attr("text","");
+  $("#inputNeighborhood").attr("text","");
+  $("#inputAddress").attr("text","");
+}
+
+ADDRESSES.requestZipCode = requestZipCode = function() { 
+  $.ajax({
+  url: $(location).attr('pathname'),
+  data: { zipcode: $("#inputZipcode").val()},
+  success: function(response) {
+       console.log("fsdfsd");
+      $("#inputState").attr("value",response.state);
+      $("#inputState").attr("text",response.state);
+      $("#inputCity").attr("value",response.city);
+      $("#inputCity").attr("text",response.city);
+      $("#inputNeighborhood").attr("value",response.neighborhood);
+      $("#inputNeighborhood").attr("text",response.neighborhood);
+      $("#inputAddress").attr("value",response.address);
+      $("#inputAddress").attr("text",response.address);
+      }
+});
+};
+
+ADDRESSES.requestTriggerZipcode = function requestTriggerZipcode() {
+  $("#inputZipcode").keyup(function() {
+    if ($(this).val().length == 9) {
+      ADDRESSES.requestZipCode();
+    }
+    else {
+      ADDRESSES.cleanFields();
+    };
+  });
+}
