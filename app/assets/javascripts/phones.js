aca555ee8ee9817d03aded22c95b1d6d63408ab2
pauladbol <pauladbol@hotmail.com>
2015-12-07 15:29:44 -0200

    #6 <@pauladbol, @gabrielpozza, @ghig, @ruas46> - arrumando layout forms cadastro oportunidades e ongs

--- app/assets/javascripts/phones.js
+++ app/assets/javascripts/phones.js
@@ -3,27 +3,22 @@ var countFields = 0;
 var count = 0;
 
  PHONES.addPhones = function addPhones(){   
-  var max_fields = 9;   
+  var max_fields = 11;   
   $(".add_field_button").click(function(e){ 
       e.preventDefault();
       if(countFields < max_fields){ 
           countFields++;
           count++;
           $(".input_fields_wrap").append('\
-            <div>\
-              <div class="col-lg-11">\
-                <input class="form-control" id="phone-ong" name="ngo[phones_attributes]['+count+'][phone_number]" type="tel">\
-              </div>\
-              <div class="col-lg-1 btn-add align-left">\
-                <a href="#" class="remove_field"><span class="glyphicon glyphicon-minus" aria-hidden="true"></a>\
-              </div>\
+            <div class="col-lg-2 form-group">\
+              <input class="form-control" id="phone-ong" name="ngo[phones_attributes]['+count+'][phone_number]" type="tel">\
             </div>');
       }
   });      
 }
 
 PHONES.removePhones = function removePhones(){ 
-  $(".input_fields_wrap").on("click",".remove_field", function(e){
-    e.preventDefault(); $(this).parent('div').parent('div').remove(); countFields--;
+  $(".remove_field").on("click", function(e){
+    e.preventDefault(); $(".input_fields_wrap").children().last().remove(); countFields--;
   });
 }
