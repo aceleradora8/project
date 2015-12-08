02e42cc8f0a61a82b9f5dba1d060913c2ba33e53
raphaelbraun <mbraun.raphael@gmail.com>
2015-12-08 09:56:19 -0200

    #0<@raphaelbraun, @pedrofontoura> - Testando configuracao do mailer em stage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'ongarium-stage.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.delivery_method = :smtp
