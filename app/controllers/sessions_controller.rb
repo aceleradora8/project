f750c5c886eecabc431a6a6c985d6de4c0558b26
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2015-12-09 09:47:44 -0200

    #0 <@ghig, @raphaelbraun> - Forçando a confirmação do cadastro ao se logar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,15 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password]) && @user.confirmed == true
       if params[:remember_me]
         cookies.permanent[:auth_token] = @user.auth_token
       else
         cookies[:auth_token] = @user.auth_token
       end
     redirect_to root_url, :notice => "Logado!"
+    elsif @user.confirmed == false
+      redirect_to login_url, :error => "Sua conta não foi ativada. Cheque seu email!"
     else
       redirect_to login_url, :error => "Email ou senha inválidos!"
     end
