7619d215373c0356e20f871995d24f9845001a13
pauladbol <pauladbol@hotmail.com>
2015-12-11 11:27:03 -0200

    #11 <@pauladbol, @andrimenegotto, @ghig> - verificação de email existente e o envio do email de confirmação

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -6,7 +6,6 @@ class VolunteersController < ApplicationController
   end
 
   def index
-    
   end
 
   def show
@@ -15,17 +14,29 @@ class VolunteersController < ApplicationController
   def create
     @user_email = User.find_by_email(volunteer_params[:user_attributes][:email])
     @volunteer = Volunteer.new(volunteer_params)
-
-    if @volunteer.save
-      redirect_to '/'
-    else
-      render 'new'
+    @volunteer.user.role = "volunteer"
+    respond_to do |format|
+      if email_exists?(@volunteer)
+        format.html {render :new, error: "O email informado já está cadastrado"}
+      elsif @volunteer.save
+        UserMailer.email_confirmation(@volunteer).deliver
+        format.html {redirect_to '/login', notice: "Voluntário cadastrado com sucesso, confirme o email para continuar"}
+      else
+        render 'new'
+      end
     end
   end
 
   private
     def volunteer_params
       params.require(:volunteer).permit(:user_id, :name, :observations, :phone, :phone1, :phone2, :user_attributes => [:email, :password, :password_confirmation])
+    end
+
+    def email_exists?(volunteer)
+      if User.find_by_email(volunteer.user.email) != nil
+        return true
+      end
+
     end
 
 end
