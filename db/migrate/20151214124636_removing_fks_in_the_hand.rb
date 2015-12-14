5720667ce0952f5a21a139772472f9ada8325d69
lucassveloso <lucas.s.veloso@hotmail.com>
2015-12-14 11:27:21 -0200

    #0 <@gabrielpozza, @lucassveloso> - removendo foreign keys e confurando seeds

---
+++ db/migrate/20151214124636_removing_fks_in_the_hand.rb
@@ -0,0 +1,13 @@
+class RemovingFksInTheHand < ActiveRecord::Migration
+  def change
+  	remove_foreign_key :ngos, :addresses
+  	remove_foreign_key :phones, :ngos
+  	remove_foreign_key :opportunities, :addresses
+	remove_foreign_key :opportunities, :ngos
+	remove_foreign_key :opportunities, :causes
+	remove_foreign_key :opportunities_skills, :skills
+   	remove_foreign_key :opportunities_skills, :opportunities	
+   	remove_foreign_key :causes_ngos, :ngos
+	remove_foreign_key :causes_ngos, :causes
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210173837) do
+ActiveRecord::Schema.define(version: 20151214124636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,13 +141,4 @@ ActiveRecord::Schema.define(version: 20151210173837) do
     t.integer  "user_id"
   end
 
-  add_foreign_key "causes_ngos", "causes"
-  add_foreign_key "causes_ngos", "ngos"
-  add_foreign_key "ngos", "addresses"
-  add_foreign_key "opportunities", "addresses"
-  add_foreign_key "opportunities", "causes"
-  add_foreign_key "opportunities", "ngos"
-  add_foreign_key "opportunities_skills", "opportunities"
-  add_foreign_key "opportunities_skills", "skills"
-  add_foreign_key "phones", "ngos"
 end
