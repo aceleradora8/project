8ecbd8dca54c53fe6ffb27f5a7715964e17522e1
gabrielpozza <gabriel.pozza@acad.pucrs.br>
2015-12-16 11:55:49 -0200

    #12 <@feliperdius, @gabrielpozza, @ruas46> - apagando rota não mais utilizada

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   get '/myopportunities' => 'opportunities#my_opportunities'
   get '/myinterests' => 'interests#my_interests'
-  get '/despilhei' => 'interests#decline_interest'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
