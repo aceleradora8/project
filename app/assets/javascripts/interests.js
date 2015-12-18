30c4f153b12593eb88bcf000f7777104e42cc408
lucassveloso <lucas.s.veloso@hotmail.com>
2015-12-18 14:39:15 -0200

    #3 <@lucassveloso, @raphaelbraun, @ruas46> - Desabilitando botão de conformar interesse quando clicado para impedir inumeros registros

--- app/assets/javascripts/interests.js
+++ app/assets/javascripts/interests.js
@@ -3,13 +3,7 @@ var INTEREST = INTEREST || {}
 
 INTEREST.validateFormOnSubmit = function validateForm(){
 	$("#form-interest").submit(function(){	
-	  validateName =VALIDATION.validateName();
-	  validateEmail = VALIDATION.validateEmail();
-	  validatePhone = VALIDATION.validatePhone();
-		if (validateName && validateEmail && validatePhone){
-			return true;
-		} else{
-			return false;
-		}
+	  $("#confirm-interest").prop( "disabled", true );
+	  $("#confirm-interest").val("Enviando...")
 	});
 }
