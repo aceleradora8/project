79f0c8da62b5346a1fb1ba8be1104c8dce2ff357
feliperdius <feliperdius@gmail.com>
2015-12-26 18:53:39 -0200

    #0 <@feliperdius> Beautify e refatoração de acordo com rubocop do arquivo application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-	# Prevent CSRF attacks by raising an exception.
-	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 	add_flash_types :error
 	helper_method :current_user
@@ -9,14 +7,13 @@ class ApplicationController < ActionController::Base
 	helper_method :volunteer_is_interested
 
 	def current_user
-		#@current_user ||= User.find(session[:user_id]) if session[:user_id]
 		@current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
 	end
 
 	def require_user
 		redirect_to login_path unless current_user
 	end
-	
+
 	def require_ngo
 		redirect_to '/' unless current_user.ngo?
 	end
@@ -30,7 +27,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def require_ngo_owner_opportunity(opportunity)
-		if current_user != nil
+		if !current_user.nil?
 			redirect_to '/' unless current_user.ngo.id == opportunity.ngo.id
 		else
 			redirect_to '/'
@@ -39,33 +36,19 @@ class ApplicationController < ActionController::Base
 
 	def store_location
 		@last_location ||= request.referer
-		puts "LAST LOCATION: #{@last_location}"
 	end
 
 	def require_user_owner_ngo(ngo)
-		if current_user.ngo.id == ngo.id
-			return true
-		else
-			return false
-		end
+		return true if current_user.ngo.id == ngo.id
 	end
 
 	def ngo_owner_of_opportunity(opportunity)
-		if (current_user != nil && current_user.ngo? && current_user.ngo.id == opportunity.ngo.id)
-			return true
-		else
-			return false
-		end
+		return true if !current_user.nil? && current_user.ngo? && current_user.ngo.id == opportunity.ngo.id
 	end
 
 	def volunteer_is_interested(opportunity)
-		if current_user != nil && current_user.volunteer? 
-			current_user.volunteer.interests.each do |interest|
-				if interest.opportunity_id == opportunity.id
-					return true
-				end
-			end
-			return false
-		end
+			return current_user.volunteer.interests.any? do |interest|
+				interest.opportunity_id == opportunity.id
+			end if !current_user.nil? && current_user.volunteer?
 	end
 end
