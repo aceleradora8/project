cdaa91e6bd01d42018b82a0d39dd03e6354d1eae
feliperdius <feliperdius@gmail.com>
2015-12-26 16:19:09 -0200

    #0 <@feliperdius> Beautify e refatoração em OPPORTUNITIES.validateFormOnSubmit

--- app/assets/javascripts/opportunities.js
+++ app/assets/javascripts/opportunities.js
@@ -1,60 +1,56 @@
 var OPPORTUNITIES = OPPORTUNITIES || {}
 
 var causesList = [];
-
 var citiesList = [];
 
-OPPORTUNITIES.validateFormOnSubmit = function validateForm(){ 
-	$("#form-opportunity").submit(function(){  
-		validateTitle = VALIDATION.validateName();
-		validateDescription =VALIDATION.validateDescription();
-		validateAddress = VALIDATION.validateZipCode();
-		validateDate = VALIDATION.validateDate();
-		if (validateTitle && validateDescription && validateAddress && validateDate) {
-		return true;
-		} else {
-		return false;
-		}
-	});
-}
-
-
-OPPORTUNITIES.requestOpportunities = function() {
-	$.get('/opportunities', {
-		causes: causesList,
-		cities: citiesList,
-		text_search: $('#text_search').val()
-	});
+OPPORTUNITIES.validateFormOnSubmit = function validateForm() {
+  $("#form-opportunity").submit(function() {
+    validateTitle = VALIDATION.validateName();
+    validateDescription = VALIDATION.validateDescription();
+    validateAddress = VALIDATION.validateZipCode();
+    validateDate = VALIDATION.validateDate();
+    if (validateTitle && validateDescription && validateAddress && validateDate) {
+      return true;
+    }
+    return false;
+  });
 };
 
+OPPORTUNITIES.requestOpportunities = function() {
+  $.get('/opportunities', {
+    causes: causesList,
+    cities: citiesList,
+    text_search: $('#text_search').val()
+  });
+};
 
 OPPORTUNITIES.getRecurrentChecked = function() {
-	$('#recurrent').on('change', function() {
-		if ($(this).is(":checked")) {
-			$(".cd_data").prop("disabled", true);
-		} else {  
-			$(".cd_data").prop("disabled", false);
-		}
-	});
-}
+  $('#recurrent').on('change', function() {
+    if ($(this).is(":checked")) {
+      $(".cd_data").prop("disabled", true);
+    } else {
+      $(".cd_data").prop("disabled", false);
+    }
+  });
+};
 
-OPPORTUNITIES.getCausesChecked = function() {  
-	$(".cd_data").prop("disabled", true);
-	$('.cb_cause').on('change', function() {
-		causesList = [];
-		$('input.cb_cause:checked').each(function() {
-			causesList.push($(this).val());
-		});
-		OPPORTUNITIES.requestOpportunities();
-	});
+OPPORTUNITIES.getCausesChecked = function() {
+  $(".cd_data").prop("disabled", true);
+  $('.cb_cause').on('change', function() {
+    causesList = [];
+    $('input.cb_cause:checked').each(function() {
+      causesList.push($(this).val());
+    });
+    OPPORTUNITIES.requestOpportunities();
+  });
 };
 
 OPPORTUNITIES.getCitiesChecked = function() {
-	$('.cb_city').on('change', function() {
-		citiesList = [];
-		$('input.cb_city:checked').each(function() {
-			citiesList.push($(this).val());
-		});
-		OPPORTUNITIES.requestOpportunities();
-	});
+  $('.cb_city').on('change', function() {
+    citiesList = [];
+    $('input.cb_city:checked').each(function() {
+      citiesList.push($(this).val());
+    });
+    OPPORTUNITIES.requestOpportunities();
+  });
 };
