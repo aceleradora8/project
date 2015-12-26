d527bde3fd53cfd3da4ead2c4302833cdc27550d
feliperdius <feliperdius@gmail.com>
2015-12-26 20:07:08 -0200

    #0 <@feliperdius> Beautify e refatoração do arquivo sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,32 @@
 class SessionsController < ApplicationController
-
   def new
     session[:return_to] ||= request.referer
   end
 
   def create
     @user = User.find_by_email(params[:session][:email])
-    if @user == nil
-      redirect_to login_url, :error => "Email não existe, por favor, cadastre-se"
+    if @user.nil?
+      redirect_to login_url, error: "Email não existe, por favor, cadastre-se"
     elsif @user && @user.authenticate(params[:session][:password]) && @user.confirmed == true
       if params[:remember_me]
         cookies.permanent[:auth_token] = @user.auth_token
       else
         cookies[:auth_token] = @user.auth_token
       end
-      if session[:return_to] != nil
-        redirect_to session.delete(:return_to), :notice => "Login realizado com sucesso!"
+      if !session[:return_to].nil?
+        redirect_to session.delete(:return_to), notice: "Login realizado com sucesso!"
       else
-        redirect_to root_url, :notice => "Login realizado com sucesso!"
+        redirect_to root_url, notice: "Login realizado com sucesso!"
       end
     elsif @user.confirmed == false
-      redirect_to login_url, :error => "Sua conta não foi ativada. Cheque seu email!"
+      redirect_to login_url, error: "Sua conta não foi ativada. Cheque seu email!"
     else
-      redirect_to login_url, :error => "Email ou senha inválidos!"
+      redirect_to login_url, error: "Email ou senha inválidos!"
     end
   end
 
   def destroy
     cookies.delete(:auth_token)
-    redirect_to root_url, :notice => "Você finalizou a sua sessão!"
+    redirect_to root_url, notice: "Você finalizou a sua sessão!"
   end
 end
-
