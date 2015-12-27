f16ecb812e464e642c6124a5694a2d91920314d8
feliperdius <feliperdius@gmail.com>
2015-12-27 14:19:16 -0200

    #0 <@feliperdius> Beautify e refatoração da model address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,14 +1,14 @@
 class Address < ActiveRecord::Base
   has_one :ngo
   has_one :opportunity
-  validates :address, :presence => true
-  validates :zipcode, :presence => true
+  validates :address, presence: true
+  validates :zipcode, presence: true
 
   def print_address
-    if self.zipcode
-      "#{self.address},  #{self.complement} -
-       #{self.neighborhood}, #{self.city} -
-       #{self.state}, #{self.country}, #{self.zipcode}"
+    if zipcode
+      "#{address},  #{complement} -
+       #{neighborhood}, #{city} -
+       #{state}, #{country}, #{zipcode}"
     else
       "Endereço não informado."
     end
