a01aced71dba223f35c8d5efbd4fb3c08b1f317a
feliperdius <feliperdius@gmail.com>
2015-12-27 14:18:00 -0200

    #0 <@feliperdius> Beautify e refatoração do arquivo volunteer_mailer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,15 +1,13 @@
 class VolunteerMailer < ApplicationMailer
-
 	default from: 'centrodeinovacaodememes@gmail.com'
 
 	def volunteer_email(volunteer, opportunity)
 		@volunteer = volunteer
 		@opportunity = opportunity
 		mail(
-			to: @volunteer.user.email, 
-			subject: "Confirmamos seu interesse na vaga #{@opportunity.title}",
-			content_type: "text/html"
-			)
-
+			 to: @volunteer.user.email,
+ 			subject: "Confirmamos seu interesse na vaga #{@opportunity.title}",
+ 			content_type: "text/html"
+		)
 	end
 end
