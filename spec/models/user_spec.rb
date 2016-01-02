9763f546b6a016b9a321069d65c6a2e2cd3c6054
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-01-02 20:37:30 -0200

    #0 <@ghig> - Criando testes unitários para a model de usuário

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,9 @@ describe User, type: :model do
 		before :each do
 			@user = User.create!(email: "teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
 			@user_new = User.new(email: "teste_new@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+			@user_volunteer = User.create!(email: "testevolunteer@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"volunteer")
+			@user_admin = User.create!(email: "testeadmin@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"admin")
+			@user_notconfirmed = User.create!(email: "testenaoconfirmado@teste.com", password:"123", confirmed: false, auth_token: "esseehmeutoken", role:"admin")
 		end
 	
 
@@ -16,6 +19,18 @@ describe User, type: :model do
 			expect(@user_new.user_exists?).to be false
 		end
 
+		it 'return true if user is volunteer' do
+			expect(@user_volunteer.volunteer?).to be true
+		end
+
+		it 'return true if user is admin' do
+			expect(@user_admin.admin?).to be true
+		end
+
+		it 'expect email to be confirmed, returning true' do
+			expect(@user_notconfirmed.email_activate).to be true
+			expect(@user.confirmed).to be(true)
+		end
 	end
 
 end
