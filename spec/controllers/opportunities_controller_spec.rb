84049a7d1b90d07f8cb634513b411010bf7bc300
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-01-02 20:34:55 -0200

    #0 <@ghig> - Criando testes unitários para a controller de Oportunidades

--- spec/controllers/opportunities_controller_spec.rb
+++ spec/controllers/opportunities_controller_spec.rb
@@ -49,4 +49,164 @@ describe OpportunitiesController, type: :controller do
         expect(assigns(:opportunities_result)).to match_array([@opportunity1])
       end
   end
+
+  describe '#new' do
+    before :each do
+        @address_poa = Address.create!(city:"POA", zipcode: "5", address:"rua")
+        @user = User.create!(email:"teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+        @ngo = Ngo.create!(address_id: @address_poa.id, name:"nome", description: "Qualquer coisa",user_id:@user.id)
+      end
+
+      it 'expect @opportunity to be a instance of Opportunity' do
+        cookies[:auth_token] = @ngo.user.auth_token
+        get :new
+        expect(assigns(:opportunity)).to be_a_new(Opportunity)
+      end
+  end
+
+  describe '#create' do
+    before :each do
+        @address_poa = Address.create!(city:"POA", zipcode: "5", address:"rua")
+        @user = User.create!(email:"teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+        @cause_animal = Cause.create!(name:"Animal")
+        @ngo = Ngo.create!(address_id: @address_poa.id, name:"nome", description: "Qualquer coisa",user_id:@user.id)
+        cookies[:auth_token] = @ngo.user.auth_token
+      end
+
+      it 'expect redirect_to opportunity if success' do
+        post :create, {opportunity: {
+          title: 'Oportunidade32',
+          description: 'qualquercoisaai',
+          address_attributes: {address:"end qlqrcoisa", zipcode:"66666-333", complement:"atualizado", state:"UP", city:"Atual", country:"Brasil", neighborhood:"Atualizado" },
+          ngo_id: @ngo.id,
+          cause_id: @cause_animal.id,
+          start_date: "2015-03-03",
+          finish_date: "2015-04-03"
+          }
+        }
+        this_opportunity = Opportunity.last.id
+        expect(response).to redirect_to("/opportunities/#{this_opportunity}")
+      end
+
+      it 'expect render new if something goes wrong' do
+        post :create, {opportunity: {
+          title: 'Oportunidade32',
+          description: 'qualquercoisaai',
+          cause_id: @cause_animal.id,
+          start_date: "2015-03-03",
+          finish_date: "2015-04-03"
+          }
+        }
+          expect(response).to render_template("new")
+      end
+
+
+      ## ISN'T WORKING :(
+      it 'expect start_date and finish_date to be nil if recurrent = 1' do
+          post :create, opportunity: {
+          title: 'Oportunidade32',
+          description: 'qualquercoisaai',
+          address_attributes: {address:"end qlqrcoisa", zipcode:"66666-333", complement:"atualizado", state:"UP", city:"Atual", country:"Brasil", neighborhood:"Atualizado" },
+          ngo_id: @ngo.id,
+          cause_id: @cause_animal.id,
+          recurrent: "1"
+          }
+        expect(assigns(:opportunity).start_date).to eq(nil)
+        expect(assigns(:opportunity).finish_date).to eq(nil)
+    end
+  end
+
+  describe '#edit' do
+    before :each do
+        @address_poa = Address.create!(city:"POA", zipcode: "5", address:"rua")
+        @user = User.create!(email:"teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+        @cause_animal = Cause.create!(name:"Animal")
+        @ngo = Ngo.create!(address_id: @address_poa.id, name:"nome", description: "Qualquer coisa",user_id:@user.id)
+        start_date = "2015-03-03"
+        finish_date = "2015-04-03"
+        @opportunity1 = Opportunity.create!(title: 'Opportunity1', description: "Qualquer coisa", address_id: @address_poa.id, ngo_id: @ngo.id, cause_id: @cause_animal.id, start_date: start_date, finish_date: finish_date)
+        cookies[:auth_token] = @ngo.user.auth_token
+    end
+
+    #TÁ TESTANDO O SET_OPPORTUNITY, NÃO O EDIT
+    it 'expect opportunity to be equals a opportunity passed by params' do
+      get :edit, id: @opportunity1.id
+      expect(assigns(:opportunity)).to eq(@opportunity1)
+    end
+
+    it 'expect to be redirect to /404 when passing a opportunity that doesnt exists' do
+      get :edit, id: -30
+      expect(response).to redirect_to("/404")
+    end
+  end
+
+  describe '#update' do
+    before :each do
+        @address_poa = Address.create!(city:"POA", zipcode: "5", address:"rua")
+        @user = User.create!(email:"teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+        @cause_animal = Cause.create!(name:"Animal")
+        @ngo = Ngo.create!(address_id: @address_poa.id, name:"nome", description: "Qualquer coisa",user_id:@user.id)
+        @start_date = "2015-03-03"
+        @finish_date = "2015-04-03"
+        @opportunity1 = Opportunity.create!(title: 'Opportunity1', description: "Qualquer coisa", address_id: @address_poa.id, ngo_id: @ngo.id, cause_id: @cause_animal.id, start_date: @start_date, finish_date: @finish_date)
+        cookies[:auth_token] = @ngo.user.auth_token
+    end
+
+    it 'redirect to opportunity if update was a success' do
+      put :update, {
+        id: @opportunity1.id, opportunity: 
+        {
+          title: 'Opportunitdade EE', description: "Qualquer coisa", address_id: @address_poa.id, ngo_id: @ngo.id, cause_id: @cause_animal.id, start_date: @start_date, finish_date: @finish_date          
+        }
+      }
+      expect(response).to redirect_to(@opportunity1)  
+    end
+
+    it 'render edit if anything goes wrong' do
+      put :update, {
+        id: @opportunity1.id, opportunity: 
+        {
+          title: nil, description: "Qualquer coisa", address_id: @address_poa.id, ngo_id: @ngo.id, cause_id: @cause_animal.id, start_date: @start_date, finish_date: @finish_date          
+        }
+      }
+      expect(response).to render_template("edit") 
+    end
+  end
+
+    describe '#destroy' do
+      before :each do
+        @address_poa = Address.create!(city:"POA", zipcode: "5", address:"rua")
+        @user = User.create!(email:"teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+        @cause_animal = Cause.create!(name:"Animal")
+        @ngo = Ngo.create!(address_id: @address_poa.id, name:"nome", description: "Qualquer coisa",user_id:@user.id)
+        @start_date = "2015-03-03"
+        @finish_date = "2015-04-03"
+        @opportunity1 = Opportunity.create!(title: 'Opportunity1', description: "Qualquer coisa", address_id: @address_poa.id, ngo_id: @ngo.id, cause_id: @cause_animal.id, start_date: @start_date, finish_date: @finish_date)
+        cookies[:auth_token] = @ngo.user.auth_token
+    end
+
+    it 'return success when Opportunity is deleted' do
+      delete :destroy, id: @opportunity1.id
+      expect(flash[:notice]).to eq("A oportunidade #{@opportunity1.title} foi removida com sucesso")
+    end
+  end
+
+    describe '#myopportunities' do
+      before :each do
+        @address_poa = Address.create!(city:"POA", zipcode: "5", address:"rua")
+        @user = User.create!(email:"teste@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"ngo")
+        @cause_animal = Cause.create!(name:"Animal")
+        @ngo = Ngo.create!(address_id: @address_poa.id, name:"nome", description: "Qualquer coisa",user_id:@user.id)
+        @start_date = "2015-03-03"
+        @finish_date = "2015-04-03"
+        @opportunity1 = Opportunity.create!(title: 'Opportunity1', description: "Qualquer coisa", address_id: @address_poa.id, ngo_id: @ngo.id, cause_id: @cause_animal.id, start_date: @start_date, finish_date: @finish_date)
+        cookies[:auth_token] = @ngo.user.auth_token
+      end
+
+    it 'expect user to be equals current_user and Ngo equals Ngo' do
+      get :my_opportunities
+      expect(assigns(:user)).to eq(@user)
+      expect(assigns(:ngo)).to eq(@ngo)
+    end
+  end
 end
