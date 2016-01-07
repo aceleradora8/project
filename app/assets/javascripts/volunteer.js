e1721de74b9ea52119f20b78f4ffcc20c37803b1
ruas46 <ghilherme.ghidorsi@acad.pucrs.br>
2016-01-07 15:53:31 -0200

    #0 <ruas46> - Corrigindo validação telefone

--- app/assets/javascripts/volunteer.js
+++ app/assets/javascripts/volunteer.js
@@ -4,6 +4,7 @@ VOLUNTEER.validateFormOnSubmit = function validateForm() {
   $("#form-volunteer").submit(function() {
     validateName = VALIDATION.validateName();
     validateEmail = VALIDATION.validateEmail();
+    validatePhone = VALIDATION.validatePhone();
     validatePassword = VALIDATION.validatePassword();
     validatePasswordConfirmation = VALIDATION.validatePasswordConfirmation();
      if ($(".input-text-error").length == 0) {  
