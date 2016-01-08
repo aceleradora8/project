77593a5ffeef75069a1c71eded26c5c3c728658e
ghilherme.ghidorsi@acad.pucrs.br <ghilherme.ghidorsi@acad.pucrs.br>
2016-01-08 14:30:00 -0200

    #0 <@ghig> - Cobrindo destroy da volunteer controller

--- spec/controllers/volunteer_controller_spec.rb
+++ spec/controllers/volunteer_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 require 'capybara/rspec'
 
-describe VolunteersController, type: :controller do 
+describe VolunteersController, type: :controller do
 	describe '#create' do
 		before :each do
 			user = User.create!(email:"teste_voluntario@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"volunteer")
@@ -12,7 +12,7 @@ describe VolunteersController, type: :controller do
 		end
 
 		it 'return error when passing a volunteer existing email' do
-			post :create, volunteer: { 
+			post :create, volunteer: {
 			user_attributes: { email: "teste_voluntario@teste.com"}
 			}
 
@@ -25,8 +25,8 @@ describe VolunteersController, type: :controller do
 			allow(UserMailer).to receive(:email_confirmation) { mailer }
 
 			post :create, volunteer: {
-			name:"name", 
-			phone:"(66)6666-6666", 
+			name:"name",
+			phone:"(66)6666-6666",
 			user_attributes: { email: "new_email@teste.com", password: "123", password_confirmation: "123"}
 			}
 
@@ -36,8 +36,8 @@ describe VolunteersController, type: :controller do
 
 		it 'render new if anything goes wrong' do
 			post :create, volunteer: {
-			name: nil, 
-			phone:"(66)6666-6666", 
+			name: nil,
+			phone:"(66)6666-6666",
 			user_attributes: { email: "new_email@teste.com", password: "123", password_confirmation: "123"}
 			}
 
@@ -52,13 +52,13 @@ describe VolunteersController, type: :controller do
 			user_new = User.new(email:"teste_novo_voluntario@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"volunteer")
 			@volunteer_new = Volunteer.new(name:"Voluntário Novo",observations:"teste", phone:"(66)6666-6666", user_id:user_new)
 			@controller = VolunteersController.new
-		end	
+		end
 
 		it 'return success message when volunteer was updated' do
-			put :update, id: @volunteer_teste.id, 
+			put :update, id: @volunteer_teste.id,
 			volunteer: {
-			name:"Voluntário atualizado", 
-			phone:"(66)6666-6666", 
+			name:"Voluntário atualizado",
+			phone:"(66)6666-6666",
 			user_attributes: { email: "new_email@teste.com", password: "123", password_confirmation: "123"}
 			}
 
@@ -67,15 +67,32 @@ describe VolunteersController, type: :controller do
 		end
 
 		it 'render new if anything goes wrong' do
-			put :update, id: @volunteer_teste.id, 
+			put :update, id: @volunteer_teste.id,
 			volunteer: {
 			name: nil,
-			phone:"(66)6666-6666", 
+			phone:"(66)6666-6666",
 			user_attributes: { email: "new_email@teste.com", password: "123", password_confirmation: "123"}
 			}
 
 			expect(response).to render_template("edit")
 		 end
+	end
+
+	describe '#destroy' do
+		before :each do
+			user = User.create!(email:"teste_voluntario@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"volunteer")
+			@volunteer_teste = Volunteer.create!(name:"Voluntário teste",observations:"teste", phone:"(66)6666-6666", user_id:user.id)
+			user_new = User.new(email:"teste_novo_voluntario@teste.com", password:"123", confirmed: true, auth_token: "esseehmeutoken", role:"volunteer")
+			@volunteer_new = Volunteer.new(name:"Voluntário Novo",observations:"teste", phone:"(66)6666-6666", user_id:user_new)
+			@controller = VolunteersController.new
+		end
+
+		it 'return success when volunteer is deleted' do
+			cookies[:auth_token] = @volunteer_teste.user.auth_token
+			delete :destroy, id: @volunteer_teste.id
+			expect(flash[:notice]).to eq("Voluntário deletado com sucesso!")
+			expect(cookies[:auth_token]).to be nil
+		end
 	end
 
 	describe '#new' do
