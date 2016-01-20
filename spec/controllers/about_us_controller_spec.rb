759ace2eee9c9473b07d0cca292be188169a0a33
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-01-20 09:26:02 -0200

    #0 <@ghig> - Criando testes para About Us Controller

--- spec/controllers/about_us_controller_spec.rb
+++ spec/controllers/about_us_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AboutUsController, type: :controller do
+  describe '#contact' do
+    it 'send email when try contact us' do
+      post :contact, {
+        type: 'NGO',
+        name: 'Qualquer Coisa',
+        email: 'abc@abc.com',
+        phone: "(51)3232-8185",
+        message: "Veloso lindo!"
+        }
 
+      expect(flash[:notice]).to eq("Mensagem entregue, obrigado por entrar em contato conosco!")
+    end
+  end
 end
