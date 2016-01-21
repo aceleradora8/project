7b020369dcea33064c3d151427c4a623be8f81f7
raphaelbraun <mbraun.raphael@gmail.com>
2016-01-21 13:54:47 -0200

    #0 <@ghig> - Removendo facebook

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-     provider :facebook, '1685367745077455', '01d21a7fbdae445378d99cb763358257', :scope => "email", info_fields: 'email,name,first_name,last_name,gender'
-end
