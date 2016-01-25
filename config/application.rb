dc035978735c9736f879615c239801dba3b876da
pauladbol <pauladbol@hotmail.com>
2016-01-25 13:50:45 -0200

    #0 <@pauladbol, @lucassveloso> - resolvendo bug nos icones na search bar

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,5 @@ module OngariumRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << Rails.root.join("app", "assets", "stylesheets","fonts")
   end
 end
